44ac6ada4a5f9d489ba7e6c16633c50a68f47f40
raamdeluxe <robin@raamdeluxe.nl>
2023-01-21 16:59:47 +0100

    camera 1.2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import ARCamera from "./ARCamera";
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+function Camera() {
+	const [stream, setStream] = useState(null);
+	const [error, setError] = useState(null);
+	const [isCameraOpen, setIsCameraOpen] = useState(false);
+
+	const openCamera = () => {
+		navigator.mediaDevices
+			.getUserMedia({ video: true })
+			.then(setStream)
+			.catch(setError);
+		setIsCameraOpen(true);
+	};
+
+	const closeCamera = () => {
+		stream.getTracks().forEach((track) => track.stop());
+		setIsCameraOpen(false);
+	};
+
+	if (error) {
+		return <p>{error.message}</p>;
+	}
 	return (
-		<div className="App">
-			<ARCamera />
+		<div>
+			{!isCameraOpen ? (
+				<button onClick={openCamera}>Open Camera</button>
+			) : (
+				<div className="video_container">
+					<video
+						playsInline
+						autoPlay
+						muted
+						ref={(ref) => ref && (ref.srcObject = stream)}
+					/>
+					<button onClick={closeCamera}>Close Camera</button>
+				</div>
+			)}
 		</div>
 	);
 }
 
-export default App;
+export default Camera;
