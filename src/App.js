e3aead3b52fb236d6b4f18031769fd47749eaad0
raamdeluxe <robin@raamdeluxe.nl>
2023-01-21 19:02:06 +0100

    changed folder structure

--- src/App.js
+++ src/App.js
@@ -1,58 +1,11 @@
-import React, { useState } from "react";
-import "./App.css";
+import Camera from "./Camera";
 
-function Camera() {
-	const [stream, setStream] = useState(null);
-	const [error, setError] = useState(null);
-	const [isCameraOpen, setIsCameraOpen] = useState(false);
-	const [cameraType, setCameraType] = useState("front");
-
-	const isMobile =
-		/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-			navigator.userAgent
-		);
-
-	const openCamera = () => {
-		const constraints = { video: { facingMode: cameraType } };
-		navigator.mediaDevices
-			.getUserMedia(constraints)
-			.then(setStream)
-			.catch(setError);
-		setIsCameraOpen(true);
-	};
-
-	const flipCamera = () => {
-		setCameraType(cameraType === "front" ? "back" : "front");
-		openCamera();
-	};
-
-	const closeCamera = () => {
-		stream.getTracks().forEach((track) => track.stop());
-		setIsCameraOpen(false);
-	};
-
-	if (error) {
-		return <p>{error.message}</p>;
-	}
-
+const App = () => {
 	return (
-		<div>
-			{!isCameraOpen ? (
-				<button onClick={openCamera}>Open Camera</button>
-			) : (
-				<div className="video_container">
-					<video
-						playsInline
-						autoPlay
-						muted
-						ref={(ref) => ref && (ref.srcObject = stream)}
-					/>
-					{isMobile && <button onClick={flipCamera}>Flip Camera</button>}
-					<button onClick={closeCamera}>Close Camera</button>
-				</div>
-			)}
+		<div className="App">
+			<Camera />
 		</div>
 	);
-}
+};
 
-export default Camera;
+export default App;
---
+++ src/Camera.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./Camera.css";
+
+function Camera() {
+	const [stream, setStream] = useState(null);
+	const [error, setError] = useState(null);
+	const [isCameraOpen, setIsCameraOpen] = useState(false);
+	const [cameraType, setCameraType] = useState("front");
+
+	const isMobile =
+		/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+			navigator.userAgent
+		);
+
+	const openCamera = () => {
+		const constraints = { video: { facingMode: cameraType } };
+		navigator.mediaDevices
+			.getUserMedia(constraints)
+			.then(setStream)
+			.catch(setError);
+		setIsCameraOpen(true);
+	};
+
+	const flipCamera = () => {
+		setCameraType(cameraType === "front" ? "back" : "front");
+		openCamera();
+	};
+
+	const closeCamera = () => {
+		stream.getTracks().forEach((track) => track.stop());
+		setIsCameraOpen(false);
+	};
+
+	if (error) {
+		return <p>{error.message}</p>;
+	}
+
+	return (
+		<div className="container">
+			{!isCameraOpen ? (
+				<button onClick={openCamera}>Open Camera</button>
+			) : (
+				<div className="video_container">
+					<video
+						playsInline
+						autoPlay
+						muted
+						ref={(ref) => ref && (ref.srcObject = stream)}
+					/>
+					{isMobile && <button onClick={flipCamera}>Flip Camera</button>}
+					<button onClick={closeCamera}>Close Camera</button>
+				</div>
+			)}
+		</div>
+	);
+}
+
+export default Camera;
